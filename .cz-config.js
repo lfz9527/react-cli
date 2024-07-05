3f43450ee89c2d309fa56f43cd1d066956ee8cc6
lifangzheng <lifangzheng@shuxin-tech.com>
2024-07-05 16:29:22 +0800

    feat(custom): 优化sz配置

--- .cz-config.js
+++ .cz-config.js
@@ -1,16 +1,37 @@
 "use strict"
 module.exports = {
   types: [
-    { value: "feat", name: "✨新增:    新的内容" },
-    { value: "fix", name: "🐛修复:    修复一个Bug" },
-    { value: "docs", name: "📝文档:    变更的只有文档" },
-    { value: "style", name: "💄格式:    空格, 分号等格式修复" },
-    { value: "refactor", name: "️♻️重构:    代码重构，注意和特性、修复区分开" },
-    { value: "perf", name: "️️⚡️性能:    提升性能" },
-    { value: "test", name: "✅测试:    添加一个测试" },
-    { value: "build", name: "🔧工具:    开发工具变动(构建、脚手架工具等)" },
-    { value: "rollback", name: "⏪回滚:    代码回退" },
-    { value: "addLog", name: "👨🏻‍💻添加log:    代码回退" },
+    { value: "feat", name: "特性:   🚀  新增功能", emoji: "🚀" },
+    { value: "fix", name: "修复:   🧩  修复缺陷", emoji: "🧩" },
+    { value: "docs", name: "文档:   📚  文档变更", emoji: "📚" },
+    {
+      value: "style",
+      name: "格式:   🎨  代码格式（不影响功能，例如空格、分号等格式修正）",
+      emoji: "🎨",
+    },
+    {
+      value: "refactor",
+      name: "重构:   ♻️  代码重构（不包括 bug 修复、功能新增）",
+      emoji: "♻️",
+    },
+    { value: "perf", name: "性能:   ⚡️  性能优化", emoji: "⚡️" },
+    {
+      value: "test",
+      name: "测试:   ✅  添加疏漏测试或已有测试改动",
+      emoji: "✅",
+    },
+    {
+      value: "build",
+      name: "构建:   📦️  构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）",
+      emoji: "📦️",
+    },
+    { value: "ci", name: "集成:   🎡  修改 CI 配置、脚本", emoji: "🎡" },
+    { value: "rollback", name: "回退:   ⏪️  回滚 commit", emoji: "⏪️" },
+    {
+      value: "other",
+      name: "其他:   🔨  对构建过程或辅助工具和库的更改（不影响源文件、测试用例）",
+      emoji: "🔨",
+    },
   ],
   // it needs to match the value for field type. Eg.: 'fix'
   /*  scopeOverrides: {
