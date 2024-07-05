7bc6ab7a50dfa15bb3319ae38a2246ac9aa0d598
lifangzheng <lifangzheng@shuxin-tech.com>
2024-07-05 17:06:29 +0800

    style(React): 修改样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 
-type Props = {
+interface Props {
   data?: string
   info?: string
   type?: string
 }
 const App: React.FC<Props> = ({ data, info, type }: Props) => {
-  return (<div></div>)
+  return <div></div>
 }
 
 export default App
