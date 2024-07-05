c004fdc49b1abc59dbc6570a498e2f6401d8f7a0
lifangzheng <lifangzheng@shuxin-tech.com>
2024-07-05 16:58:48 +0800

    style: 更新样式

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "@/design/index.less"
-import App from "./App"
+import App from "./app"
 
 const root = ReactDOM.createRoot(document.getElementById("app")!)
 root.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
 )
