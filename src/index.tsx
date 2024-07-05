5a15a54f56ef183cc73db163406bd3d7ca6a6113
lifangzheng <lifangzheng@shuxin-tech.com>
2024-07-05 14:03:12 +0800

    feat: 添加prettierrc 配置

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import '@/design/index.less'
-import App from './App'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "@/design/index.less"
+import App from "./App"
 
-const root = ReactDOM.createRoot(document.getElementById('app')!)
+const root = ReactDOM.createRoot(document.getElementById("app")!)
 // v18 的新方法
 root.render(<App />)
