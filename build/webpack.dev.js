ef275c33a0042be39a964542e90515f81d71e1ae
lifangzheng <lifangzheng@shuxin-tech.com>
2024-07-10 14:03:30 +0800

    feat: 优化 resolve.mainFields 配置

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -4,6 +4,10 @@ const base = require("./webpack.base.js")
 
 module.exports = merge(base, {
   mode: "development", // 开发模式
+  resolve: {
+    // 优先采用es6模块化方式的代码
+    mainFields: ["module", "main", "browser"],
+  },
   devServer: {
     hot: true,
     open: true, // 编译完自动打开浏览器
