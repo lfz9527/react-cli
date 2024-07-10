3e3890b6af6592c82ba888337266f6943356b4df
lifangzheng <lifangzheng@shuxin-tech.com>
2024-07-10 14:22:44 +0800

    feat: 更改文件

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "@/design/index.less"
-import App from "./app"
+import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("app")!)
 root.render(
